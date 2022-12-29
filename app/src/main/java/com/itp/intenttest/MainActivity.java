abca8d8f3c3d0983025c5f31ed71e4a189c8dd5f
dnyaneshrt <sartape51@gmail.com>
2022-12-29 09:30:25 +0530

    intial commit

---
+++ app/src/main/java/com/itp/intenttest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.itp.intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_welcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_welcome= findViewById(R.id.btn_welcome);
+
+        btn_welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent intent=new Intent(MainActivity.this,WelcomeActivity.class);
+                startActivity(intent);
+//             startActivity(new Intent(MainActivity.this,WelcomeActivity.class));
+            }
+        });
+
+    }
+}
